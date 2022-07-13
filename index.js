2b90d2916b86b9967e774b5044e7bc192da70fab
PharmaPid <michaelboesgaard@gmail.com>
2022-07-13 23:50:31 +0200

    Deleted original backend persons list

--- index.js
+++ index.js
@@ -17,34 +17,11 @@ morgan.token("json", (request) => {
 
 app.use(morgan(":method :url :response-time ms :json"));
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
-app.get("/info", (request, response) => {
+app.get("/info", (request, response, next) => {
   Person.find({}).then((persons) => {
     response.send(
       `<p>Phonebook has info for ${persons.length} people</p>
