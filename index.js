2606df6247d140056be26b4ad2a32343c62d4498
PharmaPid <michaelboesgaard@gmail.com>
2022-07-13 23:08:00 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require('cors');
-const dotenv = require('dotenv').config();
-const Person = require('./models/person')
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+const Person = require("./models/person");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static('build'))
+app.use(express.static("build"));
 
-morgan.token('json', (request) => {
-  if (request.method === 'POST') {
+morgan.token("json", (request) => {
+  if (request.method === "POST") {
     return JSON.stringify(request.body);
   }
-})
+});
 
-app.use(morgan(':method :url :response-time ms :json'));
+app.use(morgan(":method :url :response-time ms :json"));
 
 let persons = [
   {
@@ -51,73 +51,73 @@ app.get("/info", (request, response) => {
   );
 });
 
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
+app.get("/api/persons", (request, response, next) => {
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  }).catch(error => next(error));
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  Person.findById(id).then(person => {
-    response.json(person)
-  })
-  /*const person = persons.find((person) => person.id === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).json({
-      error: "person doesn't exist",
-    });*/
+app.get("/api/persons/:id", (request, response, next) => {
+  const id = request.params.id;
+  Person.findById(id)
+    .then(person => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end()
+      }
+    }).catch(error => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    persons = persons.filter(p => p.name !== person.name)
+app.put('/api/persons/:id', (request, response, next) => {
+
+  const person = {
+    name: request.body.name,
+    number: request.body.number
   }
-  response.status(204).end();
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  }).catch(error => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
-  const person = request.body;
-
-  if (persons.find((p) => p.name === person.name)) {
-    return response.status(400).json({
-      error: "person already exist",
-    });
-  }
-
-  if (!person.name) {
-    return response.status(400).json({
-      error: "person name field is missing",
-    });
-  }
-
-  if (!person.number) {
-    return response.status(400).json({
-      error: "person number field is missing",
-    });
-  }
-
-  const newId = Math.round(Math.random() * 1000000);
-  person.id = newId;
-
-  persons = persons.concat(person);
+app.post("/api/persons", (request, response, next) => {
 
   const personDb = new Person({
-    name: body.name,
-    number: body.number
+    name: request.body.name,
+    number: request.body.number
   })
 
-  personDb.save().then(savedPersonDb => {
-    response.json(savedPersonDb)
-  })
+  personDb.save().then((savedPersonDb) => {
+    response.json(savedPersonDb);
+  }).catch(error => next(error));
 });
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
